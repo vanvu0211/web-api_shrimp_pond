2170ee3bb93120cb8f2d5a3e285fd020ed14a585
vanvu0211 <vu34304@gmail.com>
2024-07-31 21:42:31 +0700

    done get environmen value

---
+++ ShrimpPond.Application/Feature/Environment/Queries/GetEnvironment/GetEnvironmentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShrimpPond.Application.Feature.Environment.Queries.GetEnvironment
+{
+    public class GetEnvironmentDTO
+    {
+        public string PondId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+    }
+}
